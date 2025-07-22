fa7a9f6b389d6fa971e7b2ddce2e0018a1101534
tgroves83 <tiffanygroves83@gmail.com>
2025-07-22 12:59:31 -0500

    Changes after addition of 2nd ServiceClient function

--- Dashboard/src/components/weatherDashboardCard/WeatherDashboardCard.tsx
+++ Dashboard/src/components/weatherDashboardCard/WeatherDashboardCard.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import Card from './Card.tsx';
-import axios from "axios";
 import WeatherArea from "./WeatherArea.tsx";
 import { fetchCurrentWeather } from "@myorg/service-client";
 import type { WeatherData } from "@myorg/service-client";
+import { fetchOpenWeather } from "@myorg/service-client";
+import type { OpenWeatherData } from "@myorg/service-client";
 
 
 interface WeatherDataProps {
@@ -47,11 +48,11 @@ const WeatherDashboardCard: React.FC = () => {
         setIsLoading(true);
 
         //1. Create a Service Client / Wrapper for FarmWeatherSys.Api (new project)
-        //      - "Create a React Service Client for my REST API"
-        //      - "GET http://localhost:5019/weatherforecast/GetWeatherForecast"
-        //      - This should return JSON data, which is transformed into a React object through the Service Client.
-        //2. Implement API methods as async calls (returning promises)
-        //3. Use promise or promise chain to get data from Service Client.
+        //      - "Create a React Service Client for my REST API" --done
+        //      - "GET http://http://localhost:8000/weatherforecast/GetWeatherForecast" --done
+        //      - This should return JSON data, which is transformed into a React object through the Service Client. --done
+        //2. Implement API methods as async calls (returning promises) --done
+        //3. Use promise or promise chain to get data from Service Client. --Not sure if it is same structure
 
         /*serviceClient.GetWeatherDataAsync()
             .promise
@@ -60,22 +61,11 @@ const WeatherDashboardCard: React.FC = () => {
                 .finally(...)
 */
         try {
-            const url = `http://localhost:8000`;
-            console.log('Making request to:', url);
+            const data: OpenWeatherData = await fetchOpenWeather();
+            console.log('Weather data:', data);
 
-            const response = await axios.get(url);
-            console.log('Response received:', response);
-            console.log('Weather data:', response.data);
-
-            if (!response.data) {
-                throw new Error('No data received from server');
-            }
-
-            // Validate the received data structure
-            if (!response.data.main || !response.data.weather || !response.data.wind) {
-                throw new Error('Invalid data structure received');
-            }
-            setWeatherData(response.data);
+            
+            setWeatherData(data);
             setStatusMessage(null);
         } catch (error) {
             console.error('Error fetching weather data:', error);
